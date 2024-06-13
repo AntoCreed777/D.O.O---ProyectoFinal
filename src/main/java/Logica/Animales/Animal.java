2a57e200e013ed6ba288b224d094df5b14284f65
AntoCreed777 <abenavides@desem.cl>
2024-06-13 17:47:33 -0400

    Separa parte grafica de logica on #2

--- src/main/java/Logica/Animales/Animal.java
+++ src/main/java/Logica/Animales/Animal.java
@@ -3,22 +3,18 @@ package Logica.Animales;
 import javax.swing.*;
 import java.util.Random;
 
-import Interfaz.GeneradorImagen;
 import Logica.Comidas.*;
 
-public abstract class Animal implements GeneradorImagen {
+public abstract class Animal {
     private int posicionX;
     private int posicionY;
     private final int pantallaLimiteInferior;
     private final int pantallaLimiteDerecho;
-    private final JLabel label;   //imagen que representa graficamente a este animal
 
 
     public Animal(int posicionXinicial, int posicionYinicial, JPanel panelHabitat) {
         pantallaLimiteInferior = panelHabitat.getPreferredSize().height;
         pantallaLimiteDerecho = panelHabitat.getPreferredSize().width;
-
-        this.label = GeneradorImagen.ImageLabel("",posicionXinicial,posicionYinicial,0,0);  //Falta implementar bien
     }
 
     public int getPosicionX() {return posicionX;}
@@ -34,26 +30,20 @@ public abstract class Animal implements GeneradorImagen {
             case 0: //Norte
                 if(posicionY > 0){
                     posicionY-=1;
-                    label.setBounds(posicionX,posicionY,0,0);   //Falta corregir las dimensiones
                 }
             case 1: //Sur
                 if(posicionY < pantallaLimiteInferior){
                     posicionY+=1;
-                    label.setBounds(posicionX,posicionY,0,0);   //Falta corregir las dimensiones
                 }
             case 2: //Este
                 if(posicionX < pantallaLimiteDerecho){
                     posicionX+=1;
-                    label.setBounds(posicionX,posicionY,0,0);   //Falta corregir las dimensiones
                 }
             case 3: //Oeste
                 if(posicionX > 0){
                     posicionX-=1;
-                    label.setBounds(posicionX,posicionY,0,0);   //Falta corregir las dimensiones
                 }
         }
-
-        label.repaint();
     }
 
     public abstract void MeterSonido();
