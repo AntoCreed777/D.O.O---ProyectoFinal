847c31c5a679ba6d79ee896030a200d7bda39aaf
kote-e <mariasanmartin2023@udec.cl>
2024-06-14 19:43:53 -0400

    correcion de errores

--- src/main/java/Interfaz/Ventana.java
+++ src/main/java/Interfaz/Ventana.java
@@ -6,7 +6,6 @@ public class Ventana extends  JFrame {
         this.setTitle("panel principal");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); // cerrar la aplicacion al apretar x
         this.setSize(1100, 670); // tamaño de la ventana
-        this.setResizable(false); // no se puede cambiar el tamaño
         this.setVisible(true);
     }
 }
