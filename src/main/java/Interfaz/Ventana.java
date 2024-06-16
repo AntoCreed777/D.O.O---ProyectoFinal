3e9c2c6440d71dd86a6bb8b87a1121ac0bf3d8f1
kote-e <mariasanmartin2023@udec.cl>
2024-06-16 02:43:15 -0400

    fondo de la ventana

--- src/main/java/Interfaz/Ventana.java
+++ src/main/java/Interfaz/Ventana.java
@@ -1,11 +1,36 @@
 package Interfaz;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+
 
 public class Ventana extends  JFrame {
+    private BufferedImage imagen;
     public Ventana(){
-        this.setTitle("panel principal");
+        this.setTitle("ventana");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); // cerrar la aplicacion al apretar x
         this.setSize(1100, 670); // tamaño de la ventana
         this.setVisible(true);
+
+        try {
+            imagen = ImageIO.read(new File("src/main/java/Interfaz/imagenes/fondo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (imagen != null) {
+                    g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+                }
+            }
+        };
+        this.setContentPane(panel);
+        this.setVisible(true);
+
     }
 }
--- src/main/java/main.java
+++
@@ -1,7 +0,0 @@
-import Interfaz.Ventana;
-
-public class Main {
-    public static void main(String[] args) {
-        new Ventana();
-    }
-}
