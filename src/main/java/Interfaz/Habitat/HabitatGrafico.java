d6984ddce877abd89529fe7474161a0f8d80aa5a
vq0001 <vquiroga2023@udec.cl>
2024-06-26 18:04:05 -0400

    Cambiar el color y posicion de editBtn en HabitatGrafico

--- src/main/java/Interfaz/Habitat/HabitatGrafico.java
+++ src/main/java/Interfaz/Habitat/HabitatGrafico.java
@@ -36,9 +36,11 @@ public class HabitatGrafico extends JFrame implements GeneradorImagen {
 
         // boton de edicion
         editBtn = new JToggleButton("MENU");
-        editBtn.setFont(new Font("monospaced",Font.PLAIN, 12));
+        editBtn.setFont(new Font("monospaced", Font.BOLD, 16));
         editBtn.setBackground(new Color(0xFFFFFF));
-        editBtn.setBounds(1015,0,70,40);
+        editBtn.setBorderPainted(false);
+        editBtn.setMargin(new Insets(0,0,0,0));
+        editBtn.setBounds(1010,5,70,40);
         editBtn.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
