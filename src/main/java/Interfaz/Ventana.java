ce23154de44ac57059cc0eef954bb5dacb2cd096
vq0001 <vquiroga2023@udec.cl>
2024-07-02 10:35:03 -0400

    Solucionar error al mostrar las imagenes de fondo de los paneles tierra y principal

--- src/main/java/Interfaz/Ventana.java
+++ src/main/java/Interfaz/Ventana.java
@@ -1,26 +1,59 @@
 package Interfaz;
 
-import Interfaz.Habitat.HabitatGrafico;
-import Logica.*;
-import Logica.TipoHabitats.HabitatMarte;
-import Logica.TipoHabitats.HabitatTierra;
-
 import javax.swing.*;
-import java.awt.*;
 
 
-public class Ventana extends  JFrame{
+public class Ventana extends JFrame{
+
+    private final JPanel panelPrincipal = new PanelPrincipal(this);
+    private final JPanel panelTierra = new PanelTierra();
+    private JPanel panelMarte;
 
     public Ventana(){
+
         this.setTitle("ventana");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); // cerrar la aplicacion al apretar x
         this.setSize(1100, 670); // tamaño de la ventana
+        this.setResizable(false);
 
-        PanelPrincipal panel = new PanelPrincipal();
-        this.setContentPane(panel);
-
+        panelTierra.setVisible(false);
+        this.add(panelPrincipal);
 
         this.setVisible(true);
     }
 
+    public void mostrarPanelPrincipal(Boolean mostrar){
+        if(mostrar) {
+            this.add(panelPrincipal);
+            panelPrincipal.setVisible(true);
+        } else {
+            this.remove(panelPrincipal);
+            panelPrincipal.setVisible(false);
+        }
+        this.repaint();
+    }
+
+    public void mostrarPanelTierra(Boolean mostrar){
+
+        if(mostrar){
+            this.add(panelTierra);
+            panelTierra.setVisible(true);
+        } else {
+            this.remove(panelTierra);
+            panelTierra.setVisible(true);
+        }
+        this.repaint();
+    }
+
+    public void mostrarPanelMarte(Boolean mostrar){
+
+        if(mostrar){
+            this.add(panelMarte);
+            panelMarte.setVisible(true);
+        } else {
+            this.remove(panelMarte);
+            panelMarte.setVisible(true);
+        }
+        this.repaint();
+    }
 }
